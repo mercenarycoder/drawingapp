de9dfdab402f457417820a3861104d600be63721
Manjeet Singh <manjeetts869@gmail.com>
2020-07-05 20:38:53 +0530

    Complete Assignment of basic doodle uploading

---
+++ app/src/main/java/com/g/baseassignments/Constant.java
@@ -0,0 +1,6 @@
+package com.g.baseassignments;
+
+public class Constant {
+    public static final String STORAGE_PATH_UPLOADS = "uploads/";
+    public static final String DATABASE_PATH_UPLOADS = "uploads";
+}
---
+++ app/src/main/java/com/g/baseassignments/Upload.java
@@ -0,0 +1,34 @@
+package com.g.baseassignments;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+/**
+ * Created by Belal on 2/23/2017.
+ */
+@IgnoreExtraProperties
+public class Upload{
+    private String mName;
+    private String mImageUrl;
+    public Upload() {
+        //empty constructor needed
+    }
+    public Upload(String name, String imageUrl) {
+        if (name.trim().equals("")) {
+            name = "No Name";
+        }
+        mName = name;
+        mImageUrl = imageUrl;
+    }
+    public String getName() {
+        return mName;
+    }
+    public void setName(String name) {
+        mName = name;
+    }
+    public String getImageUrl() {
+        return mImageUrl;
+    }
+    public void setImageUrl(String imageUrl) {
+        mImageUrl = imageUrl;
+    }
+}
